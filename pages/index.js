6d070999d09cb3fd20583ec94d40f86ef31ef826
Maham Hussain <msmahamhussain@gmail.com>
2020-07-27 01:53:17 +0500

    📦 NEW:

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
-        <title>{siteTitle}</title>
+        <title>MahamBatool</title>
       </Head>
       <section className={utilStyles.headingMd}>
         <p>I’m a Civil Engineering Candidate, an aspiring interior designer who seems to love Architecture and designs. As a published author, my work and writings have been featured in the NoteWorthy Journal at Medium.</p>
