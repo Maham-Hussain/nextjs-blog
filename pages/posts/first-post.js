6b89b73274ade47d6e4fa5a7ce711deb22b6b20a
Maham Hussain <msmahamhussain@gmail.com>
2020-07-27 01:47:57 +0500

    📦 NEW:

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -4,6 +4,7 @@ export default function FirstPost() {
     return (
         <>
             <h1>First Post</h1>
+            <p>I’m starting my very first personal blog. I can’t share how happy that makes me feel. It’s almost like an accomplishment in itself. But I won’t take all the credit here.The fact of the matter is, I received a lot of help from my brother in law, Ahmad Awais — who happens to know WordPress and building sites very well.More on this later…</p>
             <h2>
                 <Link href="/">
                     <a>Back to home</a>
