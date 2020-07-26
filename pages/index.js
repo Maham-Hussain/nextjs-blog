caa471fb5856cbe2668a8c0cc419ba7b98e05661
Maham Hussain <msmahamhussain@gmail.com>
2020-07-27 01:39:35 +0500

    📦 NEW:

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
+import Link from 'next/link'
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
 import Date from '../components/date'
 
 export default function Home({ allPostsData }) {
@@ -12,28 +12,10 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I’m a Civil Engineering Candidate, an aspiring interior designer who seems to love Architecture and designs. As a published author, my work and writings have been featured in the NoteWorthy Journal at Medium.]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>I’m a Civil Engineering Candidate, an aspiring interior designer who seems to love Architecture and designs. As a published author, my work and writings have been featured in the NoteWorthy Journal at Medium.</p>
+
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href="/posts/[id]" as={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
+      <h1>Blog Posts <Link href="/posts/first-post"><a>Click Here!</a></Link></h1>
     </Layout>
   )
 }
