3624caa91de7892bace5dee0a06bee2e1dff2479
Maham Hussain <msmahamhussain@gmail.com>
2020-07-30 00:17:37 +0500

    📦 NEW:

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Link from 'next/link'
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import Date from '../components/date'
 import { getSortedPostsData } from '../lib/posts'
 export default function Home({ allPostsData }) {
   return (
@@ -15,20 +14,7 @@ export default function Home({ allPostsData }) {
 
       </section>
       <h1>Blog Posts <Link href="/posts/first-post"><a>Click Here!</a></Link></h1>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
-          ))}
-        </ul>
-      </section>
+
     </Layout>
   )
 }
